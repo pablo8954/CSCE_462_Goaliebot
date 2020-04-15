ada9b2e6d5d2b3ee6f3d9ec30fc81da843ee937c
Pablo Say <pablo.say54@gmail.com>
2020-04-15 15:03:06 -0500

    simple change to test sensors on proto

--- sensing_ball.py
+++ sensing_ball.py
@@ -47,10 +47,81 @@ def main():
     while True:
         if run[0] == True:
             
-            left_val = left_readings()
-            right_val = right_readings()
-            print(left_val)
-            print(right_val)
+            # left_val = left_readings()
+            # right_val = right_readings()
+            # print(left_val)
+            # print(right_val)
+
+            GPIO.setwarnings(False)
+            #left sensor (from robot's perspective)
+
+            TRIG = 27
+            ECHO = 22
+
+            #print("Distance Measurement in Progress")
+
+            GPIO.setup(TRIG,GPIO.OUT)
+            GPIO.setup(ECHO,GPIO.IN)
+
+            GPIO.output(TRIG, False)
+            #print("Waiting for Sensor To Settle")
+            #time.sleep(2)
+
+            GPIO.output(TRIG, True)
+            time.sleep(.01)
+            GPIO.output(TRIG,False)
+
+            while GPIO.input (ECHO) == 0:
+                pulse_start = time.time()
+
+            while GPIO.input (ECHO) == 1:
+                pulse_end = time.time()
+
+            pulse_duration = pulse_end - pulse_start
+
+            distance = pulse_duration *17150
+
+            left_val = round(distance, 2)
+
+            print ("Left Distance:", distance, "cm")
+            #leftsensorArray.append(distance)
+            GPIO.cleanup()
+            # right sensor
+            GPIO.setmode(GPIO.BCM)
+
+            TRIG_right = 21
+            ECHO_right = 20
+
+            #print("Distance Measurement in Progress")
+
+            GPIO.setup(TRIG_right,GPIO.OUT)
+            GPIO.setup(ECHO_right,GPIO.IN)
+
+            GPIO.output(TRIG_right, False)
+            #print("Waiting for Sensor To Settle")
+            #time.sleep(2)
+
+            GPIO.output(TRIG_right, True)
+            time.sleep(0.00001)
+            GPIO.output(TRIG_right,False)
+
+            while GPIO.input (ECHO_right) == 0:
+                pulse_start = time.time()
+
+            while GPIO.input (ECHO_right) == 1:
+                pulse_end = time.time()
+
+            pulse_duration = pulse_end - pulse_start
+
+            distance = pulse_duration *17150
+
+            right_val = round(distance, 2)
+
+            print ("Right Distance:", distance, "cm\n")
+            #rightsensorArray.append(distance)
+            GPIO.cleanup()
+
+            time.sleep(.01)
 
             # move bot left if readings are skewed towards left
             if (left_val < 250 or left_val > 500) and (right_val > 250 and right_val < 500):
