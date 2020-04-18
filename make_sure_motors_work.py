40a1291e57335270fdc1ea0ba4af329dc06b1015
Pablo Say <pablo.say54@gmail.com>
2020-04-18 14:37:57 -0500

    push motor test code to see what direction positive is

--- make_sure_motors_work.py
+++ make_sure_motors_work.py
@@ -4,6 +4,11 @@ from adafruit_motorkit import MotorKit
 
 kit = MotorKit()
 
+#move positive direction 
 kit.motor1.throttle = 1.0
+kit.motor2.throttle = 1.0
 time.sleep(5)
-kit.motor1.throttle = 0
+
+#move negative direction
+kit.motor1.throttle = -1.0
+kit.motor2.throttle = -1.0
